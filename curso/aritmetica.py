2ec141fd82d651fae2fad628338d0fcca8540d48
Nelson-5553 <andresnelson2004@hotmail.com>
2025-05-08 22:36:37 -0500

    add aritmeticos

---
+++ curso/aritmetica.py
@@ -0,0 +1,29 @@
+# Operadores numericos
+
+y = 15
+x = 30
+
+# Operaciones basicas
+print("Operadores basicos")
+print("suma: ", x + y)
+print("resta: ", x - y)
+print("multiplicacion: ", x * y)
+print("division: ", x / y,"\n")
+
+# Otros operadosres
+print("Otros operadores")
+print("obtener cociente: ", x % y)
+print("obtener potencia: ",x ** y)
+print("division entero: ",x // y,"\n")
+
+# Operadores booleanos
+
+print("Operadores booleanos")
+print("Mayor que ", x > y)
+print("Menor que ",x < y)
+print("Mayor o igual ",x >= y)
+print("Menor o igual ",x <= y)
+print("igual ",x == y)
+print("desigual ",x != y)
+
+
