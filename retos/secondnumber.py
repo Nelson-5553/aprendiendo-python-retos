ba70c9b3e0c2061e3365a132a7998ca78c418fb9
Nelson-5553 <andresnelson2004@hotmail.com>
2025-05-08 20:59:16 -0500

    add folder restos and curso

---
+++ retos/secondnumber.py
@@ -0,0 +1,12 @@
+numerosAleatorios = [73, 5, 91, 42, 67, 28, 19, 86, 13, 60];
+
+mayor = numerosAleatorios[0]
+
+for i in numerosAleatorios:
+    if i > mayor:
+        mayor = i
+        # print("se descarta")
+    # elif i < mayor:
+    #     print(f"mayor encontrado es", mayor)
+    
+print(f"mayor encontrado es", mayor)
